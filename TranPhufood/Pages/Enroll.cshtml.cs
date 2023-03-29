a6ca1b63d6b48f1853c5eeb83398e96c16b5f7b9
Minh2462007 <nguyenquangminh01011980@gmail.com>
2023-03-29 21:05:56 +0700

    Add project files.

---
+++ TranPhufood/Pages/Enroll.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TranPhufood.Pages
+{
+    public class EnrollModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ TranPhufood/Pages/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TranPhufood.Pages
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
